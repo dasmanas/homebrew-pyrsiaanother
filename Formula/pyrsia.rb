d88a1ea9646a9e8ab8f5e80b88de0f24e7cba101
Manas Das <manasd@jfrog.com>
2022-10-11 21:10:28 -0700

    initial commit

--- Formula/pyrsia.rb
+++ Formula/pyrsia.rb
@@ -2,7 +2,7 @@ class Pyrsia < Formula
   desc "Open source software that helps protect the open source supply chain"
   homepage "https://pyrsia.io/"
   url "https://brewrepo.pyrsia.io/pyrsia-0.2.0.tar.gz"
-  sha256 "993e536ea7cd41b5d5b3dddeebb3333c29774255cf9d37c64d5da9e609ecfec4"
+  sha256 "fb90ba908ec0b635a02eaf5ec9edeb42c6aac68df32c081219f8c2fa50f6e51a"
   license "Apache-2.0"
   version "0.2.0"
 
