5b093f590d165ffa8eb00f5a77b877aaa4a541c5
Manas Das <manasd@jfrog.com>
2022-11-07 23:45:42 -0800

    pyrsia as service

--- Formula/pyrsia.rb
+++ Formula/pyrsia.rb
@@ -1,7 +1,7 @@
 class Pyrsia < Formula
   desc "Open source software that helps protect the open source supply chain"
   homepage "https://pyrsia.io/"
-  url "https://brewrepo.pyrsia.io/stable/x86_64/pyrsia-0.2.0+2477.tar.gz"
+  url "https://brewrepo.pyrsia.io/latest/x86_64/pyrsia-0.2.0+2477.tar.gz"
   sha256 "501e5294623d2b1fa781ae3511d682651ced9d713d9831619659148690820fae"
   license "Apache-2.0"
   version "0.2.0"
