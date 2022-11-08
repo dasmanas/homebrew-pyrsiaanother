74f4d3cdf66350df024b7f51889be7fb696ea316
Manas Das <manasd@jfrog.com>
2022-11-07 23:42:23 -0800

    pyrsia as service

--- Formula/pyrsia.rb
+++ Formula/pyrsia.rb
@@ -1,8 +1,8 @@
 class Pyrsia < Formula
   desc "Open source software that helps protect the open source supply chain"
   homepage "https://pyrsia.io/"
-  url "https://brewrepo.pyrsia.io/stable/x86_64/pyrsia-0.2.0.tar.gz"
-  sha256 "63e125e0cde0fa01f0d0b0e72e2b1d845d1512ebc07460f03895873de6c75099"
+  url "https://brewrepo.pyrsia.io/stable/x86_64/pyrsia-0.2.0+2477.tar.gz"
+  sha256 "501e5294623d2b1fa781ae3511d682651ced9d713d9831619659148690820fae"
   license "Apache-2.0"
   version "0.2.0"
 
@@ -10,6 +10,13 @@ class Pyrsia < Formula
     ENV.deparallelize
     bin.install "pyrsia"
     bin.install "pyrsia_node"
+  end
+
+  service do
+    run [opt_bin/"pyrsia_node"]
+    run_type :immediate
+    process_type :background
+    environment_variables PATH: std_service_path_env.merge!("RUST_LOG": "info,pyrsia=debug")
   end
 
   test do
